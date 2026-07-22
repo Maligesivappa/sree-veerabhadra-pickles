e30b531428f656ce54704df9a0ef388b2a361ac6
malige sivappa <maligesivappa@gmail.com>
2026-07-22 19:04:09 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,21 +9,32 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9MTiS5SG_8NKlsGtcvUqIwmxeLxjIE0e",
+  apiKey: "AIzaSyA9MIf35S8_8NKrsGtxVoqIwmxeLxjIE0",
   authDomain: "sree-veerabhadra-pickles.firebaseapp.com",
   projectId: "sree-veerabhadra-pickles",
   storageBucket: "sree-veerabhadra-pickles.firebasestorage.app",
   messagingSenderId: "921641893693",
   appId: "1:921641893693:web:17d923809dd9fc1587cf54",
-  measurementId: "G-L0VC0DCT43"
+  measurementId: "G-L8VC0BCT43"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export {
-  db, auth, collection, addDoc, getDocs, doc, updateDoc, deleteDoc,
-  onSnapshot, query, orderBy, serverTimestamp,
-  signInWithEmailAndPassword, signOut, onAuthStateChanged
+  db,
+  auth,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 };
